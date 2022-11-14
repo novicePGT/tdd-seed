d84ebe68677655e64f213e5111fc178eaca5aecc
novicePGT <dymnam@naver.com>
2022-11-14 19:23:41 +0900

    refactor: 입력받은 자동차 대수 리팩터링

--- tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
+++ tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
@@ -1,2 +1,18 @@
-package org.example.racingcar;public class PersonInput {
+package org.example.racingcar;
+
+import java.util.Scanner;
+
+public class PersonInput {
+    private Scanner scanner;
+
+    public PersonInput() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int carCount(Scanner scanner) {
+        int numberOfCars = scanner.nextInt();
+        return numberOfCars;
+    }
+
+
 }
