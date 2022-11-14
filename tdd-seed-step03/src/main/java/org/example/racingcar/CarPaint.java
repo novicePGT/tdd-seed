7f7faf0e9aea40280ec7d552a9687aea36741e35
novicePGT <dymnam@naver.com>
2022-11-14 21:13:42 +0900

    feat: enum을 사용하여 컬러저장 후 반환 기능 추가

--- tdd-seed-step03/src/main/java/org/example/racingcar/CarPaint.java
+++ tdd-seed-step03/src/main/java/org/example/racingcar/CarPaint.java
@@ -1,2 +1,12 @@
-package org.example.racingcar;public class CarPaint {
+package org.example.racingcar;
+
+enum CarPaint {
+    RED(1), ORANGE(2), YELLOW(3), GREEN(4), BLUE(5),
+    INDIGO(6), VIOLET(7), PINK(8), WHITE(9), BLACK(10);
+
+    private final int value;
+
+    CarPaint(int value) {
+        this.value = value;
+    }
 }
--- tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
+++ tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
@@ -1,6 +1,9 @@
 package org.example.racingcar;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class PersonInput {
     private Scanner scanner;
@@ -14,5 +17,10 @@ public class PersonInput {
         return numberOfCars;
     }
 
+    public List<CarPaint> carToPaintName(int inputCarCount) {
+        CarPaint[] colorArray = CarPaint.values();
+        List<CarPaint> carPaintList = Arrays.stream(colorArray).collect(Collectors.toList());
 
+        return carPaintList;
+    }
 }
