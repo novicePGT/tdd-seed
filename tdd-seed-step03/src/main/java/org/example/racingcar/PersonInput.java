e2356dbc4e8f173f801e6a144503bb187b046b35
novicePGT <dymnam@naver.com>
2022-11-14 22:40:24 +0900

    feat: 움직이는 메서드 추가

--- tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
+++ tdd-seed-step03/src/main/java/org/example/racingcar/PersonInput.java
@@ -2,14 +2,17 @@ package org.example.racingcar;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class PersonInput {
     private Scanner scanner;
+    private Random random;
 
     public PersonInput() {
         this.scanner = new Scanner(System.in);
+        this.random = new Random();
     }
 
     public int carCount(Scanner scanner) {
@@ -17,10 +20,18 @@ public class PersonInput {
         return numberOfCars;
     }
 
-    public List<CarPaint> carToPaintName(int inputCarCount) {
+    public List<CarPaint> carToPaintName() {
         CarPaint[] colorArray = CarPaint.values();
         List<CarPaint> carPaintList = Arrays.stream(colorArray).collect(Collectors.toList());
 
         return carPaintList;
+    }
+
+    public void isCarMoving() {
+        int movement = random.nextInt(10);
+
+        if (movement >= 4) {
+            System.out.println("- ");
+        }
     }
 }
