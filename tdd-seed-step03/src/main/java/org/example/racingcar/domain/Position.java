055acebb9a144e74ff68418818989fdc75d5bb9b
novicePGT <dymnam@naver.com>
2022-11-21 04:03:18 +0900

    feat: 포지션 값 증가 기능 생성

--- tdd-seed-step03/src/main/java/org/example/racingcar/domain/Position.java
+++ tdd-seed-step03/src/main/java/org/example/racingcar/domain/Position.java
@@ -1,9 +1,21 @@
-package org.example.racingcar;
+package org.example.racingcar.domain;
 
-import java.util.ArrayList;
 
 public class Position {
 
-    private ArrayList<String> valueList = new ArrayList<>();
+    private int position;
 
+    public Position(int position) {
+        this.position = position;
+    }
+
+    /* 부생성자의 개념 */
+    public Position increase() {
+        return new Position(position + 1);
+    }
+
+    /* 우승자를 찾기 위한 메서드 */
+    public int getPosition() {
+        return position;
+    }
 }
