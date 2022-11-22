9a34e2fee0add266a8b23fd939ae43c7e1310526
novicePGT <dymnam@naver.com>
2022-11-22 14:01:36 +0900

    test: 움직인 횟수 테스트(실패)

--- tdd-seed-step03/src/main/java/org/example/racingcar/domain/RacingResult.java
+++
@@ -1,5 +0,0 @@
-package org.example.racingcar;
-
-public class RacingResult {
-
-}
--- tdd-seed-step03/src/test/java/org/example/racingcar/domain/CarTest.java
+++ tdd-seed-step03/src/test/java/org/example/racingcar/domain/CarTest.java
@@ -5,11 +5,21 @@ import org.example.racingcar.strategy.RandomCarMoveStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("자동차 메서드 테스트")
 class CarTest {
+
+    @Test
+    @DisplayName("자동차 기본 포지션 테스트")
+    void 처음_포지션_테스트() {
+        Position position = new Position(0);
+        assertThat(position.getPosition()).isEqualTo(0);
+    }
+
 
     @Test
     @DisplayName("자동차 포지션 움직임 테스트")
@@ -19,5 +29,18 @@ class CarTest {
         int isMovingCar = car.move(new RandomCarMoveStrategy()).getPosition();
 
         assertThat(isMovingCar).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("자동차가 총 움직인 횟수 테스트")
+    void 움직인_횟수_테스트() {
+        Cars cars = new Cars(5);
+
+        Car car = new Car(0);
+
+        IntStream.range(0, 3)
+                        .forEach(i -> cars.move(new RandomCarMoveStrategy()));
+
+        assertThat(car.getPosition()).isEqualTo(3);
     }
 }
